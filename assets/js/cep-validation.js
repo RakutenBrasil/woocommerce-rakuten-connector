286c0d61bbbe7b802fd63d356bdc015e3d1ecb20
Joe Nascimento <joe.nascimento@rakuten.com.br>
2019-04-18 16:50:07 -0300

    Add the configuration to shipping_address at cep-validation.js

--- assets/js/cep-validation.js
+++ assets/js/cep-validation.js
@@ -65,6 +65,59 @@
                 limpa_formulário_cep();
             }
         });
+
+        //Quando o campo cep perde o foco.
+        $("#shipping_postcode").blur(function() {
+
+            //Nova variável "cep" somente com dígitos.
+            var cep = $(this).val().replace(/\D/g, '');
+
+            //Verifica se campo cep possui valor informado.
+            if (cep != "") {
+
+                //Expressão regular para validar o CEP.
+                var validacep = /^[0-9]{8}$/;
+
+                //Valida o formato do CEP.
+                if(validacep.test(cep)) {
+
+                    //Preenche os campos com "..." enquanto consulta webservice.
+                    $("#shipping_address_1").val("...");
+                    $("#shipping_address_2").val("");
+                    $("#shipping_district").val("...");
+                    $("#shipping_city").val("...");
+
+                    //Consulta o webservice viacep.com.br/
+                    $.getJSON("https://viacep.com.br/ws/"+ cep +"/json/?callback=?", function(dados) {
+
+                        if (!("erro" in dados)) {
+                            //Atualiza os campos com os valores da consulta.
+                            $("#shipping_address_1").val(dados.logradouro);
+                            $("#shipping_address_2").val("");
+                            $("#shipping_district").val(dados.bairro);
+                            $("#shipping_city").val(dados.localidade);
+                            $("#shipping_state").val(dados.uf).change();
+                            $("#shipping_address_number").focus();
+
+                        } //end if.
+                        else {
+                            //CEP pesquisado não foi encontrado.
+                            limpa_formulário_cep();
+                            alert("CEP não encontrado.");
+                        }
+                    });
+                } //end if.
+                else {
+                    //cep é inválido.
+                    limpa_formulário_cep();
+                    alert("Formato de CEP inválido.");
+                }
+            } //end if.
+            else {
+                //cep sem valor, limpa formulário.
+                limpa_formulário_cep();
+            }
+        });
     });
 
 }( jQuery ));
